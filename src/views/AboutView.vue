8542aac5314770a96dfb7bc371f6cdacedc21ac7
Alexandre-Ramirez <alexandre.ramirez@eduvaud.ch>
2025-12-27 18:09:10 +0100

    final page about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A propos</h1>
+    <p>Ce site permet de visualiser la météo actuelle</p> <div class="info">
+      <p><strong>Aide :</strong> Selon la documentation disponible sur le site, il est possible de récupérer la météo d'une ville à l'aide de l'URL :</p>
+      <code>https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr</code> </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.info {
+  margin-top: 20px;
+  padding: 15px;
+  background-color: #f4f4f4;
+  border-radius: 5px;
+  text-align: left;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+}
+code {
+  word-break: break-all;
+  color: #d63384;
 }
 </style>
